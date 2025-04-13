5b0f862f51d53402a7b454b4bc32700516f85687
iiTzX1sRol <vovavovakak@gmail.com>
2025-04-13 19:56:58 +0300

    task 4 fix 3

--- task4/index.php
+++ task4/index.php
@@ -40,35 +40,38 @@ if ($_SERVER['REQUEST_METHOD'] == 'GET') {
     include('form.html');
     exit();
 }
+
 $errors = [];
+
 if (empty($_POST['name'])) {
     $errors['name'] = 'Поле ФИО обязательно для заполнения';
-}
-elseif (!preg_match('/^[а-яА-ЯёЁa-zA-Z\s]+$/u', $_POST['name'])) {
+} elseif (!preg_match('/^[а-яА-ЯёЁa-zA-Z\s]+$/u', $_POST['name'])) {
     $errors['name'] = 'ФИО может содержать только буквы и пробелы';
 }
+
 if (empty($_POST['phone'])) {
     $errors['phone'] = 'Поле Телефон обязательно для заполнения';
-}
-elseif (!preg_match('/^(\+7|8)\d{10}$/', $_POST['phone'])) {
+} elseif (!preg_match('/^(\+7|8)\d{10}$/', $_POST['phone'])) {
     $errors['phone'] = 'Телефон должен быть в формате +7XXXXXXXXXX или 8XXXXXXXXXX';
 }
+
 if (empty($_POST['email'])) {
     $errors['email'] = 'Поле Email обязательно для заполнения';
-}
-elseif (!filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
+} elseif (!filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
     $errors['email'] = 'Введите корректный email адрес';
 }
+
 if (empty($_POST['birthday'])) {
     $errors['birthday'] = 'Поле Дата рождения обязательно для заполнения';
 }
+
 if (empty($_POST['gender']) || !in_array($_POST['gender'], ['Мужской', 'Женский'])) {
     $errors['gender'] = 'Укажите пол';
 }
+
 if (empty($_POST['language'])) {
     $errors['language'] = 'Выберите хотя бы один язык программирования';
-}
-else {
+} else {
     $validLanguages = ['Pascal', 'C', 'C++', 'JavaScript', 'PHP', 'Python', 'Java', 'Haskel', 'Clojure', 'Prolog', 'Scala', 'Go'];
     foreach ($_POST['language'] as $lang) {
         if (!in_array($lang, $validLanguages)) {
@@ -77,6 +80,7 @@ else {
         }
     }
 }
+
 if (empty($_POST['contract'])) {
     $errors['contract'] = 'Необходимо подтвердить ознакомление с контрактом';
 }
@@ -87,6 +91,7 @@ if (!empty($errors)) {
     header('Location: index.php');
     exit();
 }
+
 $form_data = [
     'name' => $_POST['name'],
     'phone' => $_POST['phone'],
@@ -105,7 +110,9 @@ try {
         PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
         PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8mb4'
     ]);
+    
     $db->beginTransaction();
+    
     $stmt = $db->prepare("INSERT INTO applications (name, phone, email, birthday, gender, biography) 
                          VALUES (:name, :phone, :email, :birthday, :gender, :biography)");
     $stmt->execute([
@@ -116,13 +123,17 @@ try {
         ':gender' => $_POST['gender'],
         ':biography' => $_POST['biography'] ?? null
     ]);
+    
     $appId = $db->lastInsertId();
+    
     $stmt = $db->prepare("INSERT INTO application_languages (application_id, language_id)
                           SELECT :app_id, id FROM programming_languages WHERE name = :lang_name");
     foreach ($_POST['language'] as $lang) {
         $stmt->execute([':app_id' => $appId, ':lang_name' => $lang]);
     }
+    
     $db->commit();
+    
     header('Location: ?save=1');
     exit();
 } catch (PDOException $e) {
